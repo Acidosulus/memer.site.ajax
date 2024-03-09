1530de04ec6654dabe9484351fb30bd6887d5e83
Acidosulus <acidos@yandex.ru>
2024-03-09 18:09:22 +0700

    2024-03-09 18:09:22

--- APIServer/run_FastAPI_server.sh
+++ APIServer/run_FastAPI_server.sh
@@ -1,1 +1,2 @@
+cd /home/acidos/voc/memer.site/APIServer
 uvicorn APIServer:app --reload --host 0.0.0.0 --port 9001
--- voc/run_server.sh
+++ voc/run_server.sh
@@ -1,2 +1,3 @@
+cd /home/acidos/voc/memer.site/voc
 python3 manage.py runserver 0.0.0.0:13548
 #uvicorn voc.asgi:application --reload --host 0.0.0.0 --port 9002
