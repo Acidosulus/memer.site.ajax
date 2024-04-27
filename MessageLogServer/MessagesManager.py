a49e6591bfa3e3b86412af9676138ae830c8079e
Acidosulus <acidos@yandex.ru>
2024-04-28 01:53:48 +0700

    2024-04-28 01:53:48

--- MessageLogServer/MessagesManager.py
+++ MessageLogServer/MessagesManager.py
@@ -1,6 +1,10 @@
 from bson import ObjectId
 from pymongo import MongoClient
 from datetime import datetime, timedelta
+from rich import print
+
+
+
 
 class MessageManager:
 	def __init__(self, db_url, db_name, collection_name):
@@ -23,7 +27,7 @@ class MessageManager:
 		return list(messages)
 
 	def get_messages_since_id(self, username, message_id):
-		messages = self.collection.find({"username": username, "_id": {"$gt": ObjectId(message_id)}})
+		messages = self.collection.find({"username": username, "_id": {"$gt": message_id}})
 		return list(messages)
 
 	def get_messages_since_date(self, username, start_date):
@@ -36,20 +40,25 @@ if __name__ == "__main__":
 	message_manager = MessageManager("mongodb://localhost:27017/", "Memer", "messages")
 
 	# Сохранение сообщения в базу данных
-	message_manager.save_message(username="Test", icon_name="first", message_text="Hello, world!4", hyperlink="https://example3.com")
-	message_manager.save_message(username="Test", icon_name="second", message_text="Hello, world!5", hyperlink="https://example4.com")
-	message_manager.save_message(username="Test", icon_name="third", message_text="Hello, world!6", hyperlink="https://example5.com")
+	# message_manager.save_message(username="Test", icon_name="first", message_text="Hello, world!4", hyperlink="https://example3.com")
+	# message_manager.save_message(username="Test", icon_name="second", message_text="Hello, world!5", hyperlink="https://example4.com")
+	# message_manager.save_message(username="Test", icon_name="third", message_text="Hello, world!6", hyperlink="https://example5.com")
 
 	# Получение последних сообщений пользователя
-	last_messages = message_manager.get_last_messages("test", 5)
-	print("Last messages:")
-	for message in last_messages:
-		print(message)
+	# last_messages = message_manager.get_last_messages("test", 5)
+	# print("Last messages:")
+	# for message in last_messages:
+	# 	print(message)
 
+	# messages_since_id = message_manager.get_messages_since_date("Test", datetime.now() - timedelta(hours=2))
+	
 	# Получение сообщений пользователя с идентификатором больше заданного
-	since_message_id = '662d04a64e01e7a079a8cce1' # Предположим, что это идентификатор сообщения
-	# messages_since_id = message_manager.get_messages_since_id("Test", since_message_id)
-	messages_since_id = message_manager.get_messages_since_date("Test", datetime.now() - timedelta(hours=6))
-	print("Messages since message ID", since_message_id, ":")
+	since_message_id = ObjectId('662d04a64e01e7a079a8cce1') # Предположим, что это идентификатор сообщения
+	messages_since_id = message_manager.get_messages_since_id("Test", since_message_id)
+	
+	# print("Messages since message ID", since_message_id, ":")
 	for message in messages_since_id:
+		print()
 		print(message)
+	print()
+	print({'count':len(messages_since_id)})
