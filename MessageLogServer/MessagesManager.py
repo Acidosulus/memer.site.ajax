0df951740ed490577008ec7144b9cf9b01679150
Acidosulus <acidos@yandex.ru>
2024-04-28 00:04:23 +0700

    2024-04-28 00:04:23

--- MessageLogServer/MessagesManager.py
+++ MessageLogServer/MessagesManager.py
@@ -23,7 +23,7 @@ class MessageManager:
 		return list(messages)
 
 	def get_messages_since_id(self, username, message_id):
-		messages = self.collection.find({"username": username, "_id": {"$gt": message_id}})
+		messages = self.collection.find({"username": username, "_id": {"$gt": ObjectId(message_id)}})
 		return list(messages)
 
 	def get_messages_since_date(self, username, start_date):
@@ -36,9 +36,9 @@ if __name__ == "__main__":
 	message_manager = MessageManager("mongodb://localhost:27017/", "Memer", "messages")
 
 	# Сохранение сообщения в базу данных
-	# message_manager.save_message(username="Test", icon_name="smiley", message_text="Hello, world!", hyperlink="https://example.com")
-	# message_manager.save_message(username="Test", icon_name="sad", message_text="Hello, world!2", hyperlink="https://example1.com")
-	# message_manager.save_message(username="Test", icon_name="welcome", message_text="Hello, world!3", hyperlink="https://example2.com")
+	message_manager.save_message(username="Test", icon_name="first", message_text="Hello, world!4", hyperlink="https://example3.com")
+	message_manager.save_message(username="Test", icon_name="second", message_text="Hello, world!5", hyperlink="https://example4.com")
+	message_manager.save_message(username="Test", icon_name="third", message_text="Hello, world!6", hyperlink="https://example5.com")
 
 	# Получение последних сообщений пользователя
 	last_messages = message_manager.get_last_messages("test", 5)
@@ -47,8 +47,9 @@ if __name__ == "__main__":
 		print(message)
 
 	# Получение сообщений пользователя с идентификатором больше заданного
-	since_message_id = 100 # Предположим, что это идентификатор сообщения
-	messages_since_id = message_manager.get_messages_since_date("JohnDoe", datetime.now() - timedelta(hours=6))
+	since_message_id = '662d04a64e01e7a079a8cce1' # Предположим, что это идентификатор сообщения
+	# messages_since_id = message_manager.get_messages_since_id("Test", since_message_id)
+	messages_since_id = message_manager.get_messages_since_date("Test", datetime.now() - timedelta(hours=6))
 	print("Messages since message ID", since_message_id, ":")
 	for message in messages_since_id:
 		print(message)
